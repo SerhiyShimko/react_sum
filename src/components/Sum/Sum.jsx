5da2dddf71747234ca28f4ac64725d3639db4dd6
Serhii Shimko <shimkosergij2010@gmail.com>
2026-03-16 18:04:17 +0200

    completed the task

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,7 +1,3 @@
 export const Sum = function name({ a = 0, b = 0 }) {
-  return (
-    <p>
-      `Sum of ${a} and ${b} is ${a + b}`
-    </p>
-  );
+  return <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>;
 };
